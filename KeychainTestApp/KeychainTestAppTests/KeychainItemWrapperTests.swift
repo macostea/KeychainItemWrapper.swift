0f87650d9ddf9777c5ed508b4e870dc5dd9b8a91
Mihai Costea <macostea@users.noreply.github.com>
2016-09-30 14:04:55 +0300

    Tests enhancement (#6)
    * Add tests and travis yml
    
    * Remove testExample method
    
    * Fix scheme name
    
    * Use xcodebuild instead of xctool on travis;
    See https://github.com/facebook/xctool/issues/704 for details
    
    * Add destination string
    
    * Remove extra whitespace

---
+++ KeychainTestApp/KeychainTestAppTests/KeychainItemWrapperTests.swift
@@ -0,0 +1,39 @@
+//
+//  KeychainItemWrapperTests.swift
+//  KeychainTestApp
+//
+//  Created by MihaiC on 30/09/2016.
+//  Copyright © 2016 Mihai Costea. All rights reserved.
+//
+
+import XCTest
+@testable import KeychainTestApp
+
+
+class KeychainItemWrapperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSetItem() {
+        let keychainItemWrapper = KeychainItemWrapper(identifier: "identifier", accessGroup: nil)
+        keychainItemWrapper["superSecretKey"] = "aSuperSecretValue" as AnyObject?
+        
+        XCTAssertEqual(keychainItemWrapper["superSecretKey"] as! String!, "aSuperSecretValue")
+    }
+    
+    func testGetItem() {
+        let keychainItemWrapper = KeychainItemWrapper(identifier: "identifier", accessGroup: nil)
+        keychainItemWrapper["superSecretKey"] = "testvalue" as AnyObject?
+        
+        let otherKeychainItemWrapper = KeychainItemWrapper(identifier: "identifier", accessGroup: nil)
+        XCTAssertEqual(otherKeychainItemWrapper["superSecretKey"] as! String!, "testvalue")
+    }
+}
